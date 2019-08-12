6556a53d06ac4f061293d6e92c9df1c54f2a2781
bdshi <bdshi@dfocuspace.com>
2019-08-12 16:17:46 +0800

    feat: 修改测试,打开并发发送jps

--- application.client/src/main/java/com/dfocus/pmsg/client/StompClient.java
+++ application.client/src/main/java/com/dfocus/pmsg/client/StompClient.java
@@ -1,8 +1,10 @@
 package com.dfocus.pmsg.client;
 
 import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.util.StringUtils;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
@@ -13,6 +15,8 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author: baozi
@@ -23,11 +27,11 @@ public class StompClient {
 
 	private static String TOKEN = "eyJ1c2VyTmFtZSI6Ind1ZGl4aWFvYmFvemkiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1Njc1OTE1NDYsInVzZXJOYW1lIjoid3VkaXhpYW9iYW96aSIsImlhdCI6MTU2NDk5OTU0Nn0.dEJzjgwwZCL6qh3NtluVSo0uZZZdUEzrNF2pLsUxprVOSE-pzaUVlOw2EmntXd4IpFs3qI0IwA4F51VOFIX65lc1RoX93AFeb44CYt9JpXKcGtGYWQr2D4nsNMaS7je8abtastBC8QIInCYtC7s8tvaAQRzYTvCZmSM8vtgu06g";
 
-	private static String REQ_URL = "ws://localhost:8080/msg-center/websocket?token=" + TOKEN + "&projectId=fm";
+	private static String REQ_URL = "ws://139.217.99.53:8080/msg-center/websocket?token=" + TOKEN + "&projectId=fm";
 
 	public static void main(String[] args) throws InterruptedException {
 
-		long clientThreadNum = 5000;
+		long clientThreadNum = 1000;
 		if (args.length > 0 && !StringUtils.isEmpty(args[0])) {
 			clientThreadNum = Long.valueOf(args[0]);
 			System.out.println("客户端建立链接数:" + clientThreadNum);
@@ -42,13 +46,38 @@ public class StompClient {
 		transports.add(new WebSocketTransport(new StandardWebSocketClient()));
 		WebSocketClient transport = new SockJsClient(transports);
 		WebSocketStompClient stompClient = new WebSocketStompClient(transport);
-
 		stompClient.setMessageConverter(new StringMessageConverter());
-
 		StompSessionHandler sessionHandler = new MyStompSessionHandler();
+
+		ExecutorService executorService = Executors.newCachedThreadPool();
+
 		while (clientThreadNum-- > 0) {
+
 			Thread.sleep(1);
-			stompClient.connect(REQ_URL, sessionHandler);
+
+			executorService.submit(() -> {
+				int waitTime = 1000;
+				while (true) {
+					try {
+						ListenableFuture<StompSession> connect = stompClient.connect(REQ_URL, sessionHandler);
+						StompSession stompSession = connect.get();
+						if (stompSession != null) {
+							break;
+						}
+					}
+					catch (Exception e) {
+						try {
+							Thread.sleep(waitTime);
+							waitTime += 1000;
+						}
+						catch (InterruptedException e1) {
+							e1.printStackTrace();
+						}
+						e.printStackTrace();
+					}
+
+				}
+			});
 		}
 		// Don't close immediately.
 		new Scanner(System.in).nextLine();
