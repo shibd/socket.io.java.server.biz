1726a9e70c9a7657c8decbc1609e2e477ebeead5
shibd <wudixiaolongbao@gmail.com>
2019-08-19 11:11:02 +0800

    fix: 动态代开或关闭跨域配置

--- application/src/main/java/com/dfocus/pmsg/config/WebAppConfig.java
+++ application/src/main/java/com/dfocus/pmsg/config/WebAppConfig.java
@@ -1,5 +1,6 @@
 package com.dfocus.pmsg.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @date: 2019/8/16 15:54
  * @description: 解决跨域,打开后静态页面不正常,注释掉
  */
+@ConditionalOnProperty(value = "cors.enable", havingValue = "true")
 @Configuration
 @EnableWebMvc
 @CrossOrigin
