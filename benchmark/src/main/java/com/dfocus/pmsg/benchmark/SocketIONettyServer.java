a2ab76648f6922568dc5e0ffc01fab37a27b2c7a
shibd <wudixiaolongbao@gmail.com>
2019-08-28 18:55:26 +0800

    fix: 测试1

---
+++ benchmark/src/main/java/com/dfocus/pmsg/benchmark/SocketIONettyServer.java
@@ -0,0 +1,126 @@
+package com.dfocus.pmsg.benchmark;
+
+import com.corundumstudio.socketio.*;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author: baozi
+ * @date: 2019/8/28 11:43
+ * @description:
+ */
+public class SocketIONettyServer {
+
+	public static void main(String[] args) {
+		/*
+		 * 创建Socket，并设置监听端口
+		 */
+		Configuration config = new Configuration();
+		// 设置主机名
+		config.setHostname("localhost");
+		// 设置监听端口
+		config.setPort(9092);
+		// 协议升级超时时间（毫秒），默认10秒。HTTP握手升级为ws协议超时时间
+		config.setUpgradeTimeout(10000);
+		// Ping消息超时时间（毫秒），默认60秒，这个时间间隔内没有接收到心跳消息就会发送超时事件
+		config.setPingTimeout(180000);
+		// Ping消息间隔（毫秒），默认25秒。客户端向服务器发送一条心跳消息间隔
+		config.setPingInterval(60000);
+		// 连接认证，这里使用token更合适
+		config.setAuthorizationListener(new AuthorizationListener() {
+			@Override
+			public boolean isAuthorized(HandshakeData data) {
+				String token = data.getSingleUrlParam("token");
+				if (token.equals("test_token")) {
+					return true;
+				}
+				return false;
+			}
+		});
+
+		final SocketIOServer server = new SocketIOServer(config);
+
+		/*
+		 * 添加连接监听事件，监听是否与客户端连接到服务器
+		 */
+		server.addConnectListener(new ConnectListener() {
+			@Override
+			public void onConnect(SocketIOClient client) {
+				// 判断是否有客户端连接
+				if (client != null) {
+					System.out.println(("连接成功。clientId=" + client.getSessionId().toString()));
+
+					String projectId = client.getHandshakeData().getSingleUrlParam("projectId");
+					String topic = client.getHandshakeData().getSingleUrlParam("topic");
+					String token = client.getHandshakeData().getSingleUrlParam("token");
+
+					System.out.println("projectId:" + projectId + "  topic:" + topic + "  token:" + token);
+
+					// 构造namespace
+					server.addNamespace("/" + projectId);
+
+					// 加入到一个room
+					client.joinRoom(topic);
+
+				}
+				else {
+					System.out.println(("并没有人连接上。。。"));
+				}
+			}
+		});
+
+		/*
+		 * 添加监听事件，监听客户端的事件 1.第一个参数eventName需要与客户端的事件要一致 2.第二个参数eventClase是传输的数据类型
+		 * 3.第三个参数listener是用于接收客户端传的数据，数据类型需要与eventClass一致
+		 */
+		server.addEventListener("login", LoginRequest.class, new DataListener<LoginRequest>() {
+			@Override
+			public void onData(SocketIOClient client, LoginRequest data, AckRequest ackRequest) {
+				System.out.println(("接收到客户端login消息：" + data.getBody()));
+
+				// check is ack requested by client, but it's not required check
+				if (ackRequest.isAckRequested()) {
+					// send ack response with data to client
+					ackRequest.sendAckData("已成功收到客户端登录请求", "yeah");
+				}
+				// 向客户端发送消息
+				List<String> list = new ArrayList<>();
+				list.add("登录成功，sessionId=" + client.getSessionId());
+				// 第一个参数必须与eventName一致，第二个参数data必须与eventClass一致
+				String topic = client.getHandshakeData().getSingleUrlParam("topic");
+				server.getRoomOperations(topic).sendEvent("login", list.toString());
+			}
+		});
+
+		// 启动服务
+		server.start();
+
+		while (true) {
+			try {
+				Thread.sleep(10000);
+
+				SocketIONamespace fmNamespace = server.getNamespace("/fm");
+				if (fmNamespace != null) {
+					System.out.println("发送fm消息,客户端数量:" + fmNamespace.getAllClients().size());
+					fmNamespace.getBroadcastOperations().sendEvent("test", "are you live fm?");
+				}
+
+				SocketIONamespace amNamespace = server.getNamespace("/am");
+				if (amNamespace != null) {
+					System.out.println("发送am消息,客户端数量:" + amNamespace.getAllClients().size());
+					amNamespace.getBroadcastOperations().sendEvent("test", "are you live am?");
+				}
+
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
--- service/src/main/java/com/dfocus/pmsg/service/atom/impl/WsMessageServiceImpl.java
+++ service/src/main/java/com/dfocus/pmsg/service/atom/impl/WsMessageServiceImpl.java
@@ -1,11 +1,18 @@
 package com.dfocus.pmsg.service.atom.impl;
 
+import com.corundumstudio.socketio.BroadcastOperations;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIONamespace;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.protocol.Packet;
 import com.dfocus.pmsg.facade.model.WsMessage;
 import com.dfocus.pmsg.service.atom.IWsMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * @author: baozi
@@ -16,18 +23,48 @@ import org.springframework.stereotype.Service;
 @Service
 public class WsMessageServiceImpl implements IWsMessageService {
 
+
 	@Autowired
-	private SimpMessagingTemplate messagingTemplate;
+	SocketIOServer socketIOServer;
+
+    // SocketIOServer --> 多SocketIONamespace(NameSpace) --> 多Room（BroadcastOperations） --> 多SocketIOClient
+    //                                                  send(event, )          send（event, )
+
+    // NameSpaceClient 拥有 NameSpace
+
+
+
+
+
+
+    // server --> namespaces
+//        SocketIONamespace namespace = server.getNamespace("sdsd");
+//
+//        // namespace --> rooms
+//        BroadcastOperations roomOperations = namespace.getRoomOperations();
+//        roomOperations.send();
+//
+//        // room --> clients
+//        Collection<SocketIOClient> clients = roomOperations.getClients();
+//        for (SocketIOClient client : clients) {
+//            client.send();
+//		}
+
 
 	@Override
 	public boolean sendToTopic(WsMessage.WsTopicMessage message) {
 
-		log.info("project:{}|group:{}|message:{}", message.getProjectId(), message.getTopic(), message.getPlayLoad());
+        log.info("project:{}|group:{}|message:{}", message.getProjectId(), message.getTopic(), message.getPlayLoad());
 
-		String destination = String.format("/topic/%s/%s", message.getProjectId(), message.getTopic());
+        SocketIONamespace namespace = socketIOServer.getNamespace(message.getProjectId());
+        if (namespace == null) {
+            log.error("该项目下没发现客户端");
+		}
 
-		// 发送消息
-		messagingTemplate.convertAndSend(destination, message.getPlayLoad());
+
+        BroadcastOperations roomOperations = namespace.getRoomOperations(message.getTopic());
+
+        roomOperations.sendEvent(message.getTopic(), message.getPlayLoad());
 
 		return true;
 	}
@@ -37,10 +74,14 @@ public class WsMessageServiceImpl implements IWsMessageService {
 
 		log.info("project:{}|user:{}|message:{}", message.getProjectId(), message.getUser(), message.getPlayLoad());
 
-		String destination = String.format("/queue/%s", message.getProjectId());
+        SocketIONamespace namespace = socketIOServer.getNamespace(message.getProjectId());
+        if (namespace == null) {
+            log.error("该项目下没发现客户端");
+        }
 
-		// 发送消息
-		messagingTemplate.convertAndSendToUser(message.getUser(), destination, message.getPlayLoad());
+        SocketIOClient client = namespace.getClient(UUID.randomUUID());
+
+        client.sendEvent(message.getUser(), message.getPlayLoad());
 
 		return true;
 	}
