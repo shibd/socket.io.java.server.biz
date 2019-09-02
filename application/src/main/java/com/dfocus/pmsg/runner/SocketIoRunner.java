9c05bfa9cfb55509ba0d4731a6011d9503fa2330
shibd <wudixiaolongbao@gmail.com>
2019-09-02 17:50:12 +0800

    fix: 调整配置使代码可以单元测试

--- application/src/main/java/com/dfocus/pmsg/runner/SocketIoRunner.java
+++ application/src/main/java/com/dfocus/pmsg/runner/SocketIoRunner.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +22,7 @@ import java.util.Map;
 @Slf4j
 @Order(1000)
 @Component
+@ConditionalOnProperty(value = "socket-io.enable", havingValue = "true", matchIfMissing = true)
 public class SocketIoRunner implements CommandLineRunner {
 
 	@Autowired
