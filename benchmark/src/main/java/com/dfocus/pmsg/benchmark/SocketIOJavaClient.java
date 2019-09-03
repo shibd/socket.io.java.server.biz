0af0c310b0a9176049fab258442554e531390d75
shibd <wudixiaolongbao@gmail.com>
2019-09-03 11:37:51 +0800

    feat: 增加注释,修改token

--- benchmark/src/main/java/com/dfocus/pmsg/benchmark/SocketIOJavaClient.java
+++ benchmark/src/main/java/com/dfocus/pmsg/benchmark/SocketIOJavaClient.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 public class SocketIOJavaClient {
 
-	public static final String token = "ayJ1c2VyTmFtZSI6ImJhb3ppIiwiYWxnIjoiUlMyNTYifQ.eyJleHAiOjE1Njc1OTE0OTUsInVzZXJOYW1lIjoiYmFvemkiLCJpYXQiOjE1NjQ5OTk0OTV9.LT_BS85442djhR8YWbRVOob6yNFRYZomMBWLQkIomFfppTOU1-g3R7OmitDqwDGmTFjj12ElUch0Ov9GkP46mxX1eWuDZxXssOp8ePtoNX69exYgfXO0wRX-C4W15oPQra_PhWbqn0RxOoIGN1Uxn4CPjQ8s8dFgwP4MJjrnE3g";
+	public static final String token = "eyJ1c2VyTmFtZSI6ImJhb3ppIiwiYWxnIjoiUlMyNTYifQ.eyJleHAiOjE1Njc1OTE0OTUsInVzZXJOYW1lIjoiYmFvemkiLCJpYXQiOjE1NjQ5OTk0OTV9.LT_BS85442djhR8YWbRVOob6yNFRYZomMBWLQkIomFfppTOU1-g3R7OmitDqwDGmTFjj12ElUch0Ov9GkP46mxX1eWuDZxXssOp8ePtoNX69exYgfXO0wRX-C4W15oPQra_PhWbqn0RxOoIGN1Uxn4CPjQ8s8dFgwP4MJjrnE3g";
 
 	private static final String projectIdFm = "fm";
 
